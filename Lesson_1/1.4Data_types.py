216ccf66d8a1bccf376564297e60fcfb8cf0696b
adagioo1993 <adagioo1993@gmail.com>
2020-04-03 21:58:23 +0200

    daily update

--- Lesson_1/1.4Data_types.py
+++ Lesson_1/1.4Data_types.py
@@ -34,3 +34,39 @@ print(ex_1)
 print(ex_1.keys())
 print(list(ex_1.keys()))
 print(ex_1.values())
+
+#Is we want to check the key of the value, we can do it like this
+if "Cofee" in ex_1.keys(): 
+        print("Present, ", end =" ") 
+        print("value =", ex_1["Cofee"])
+#If we want to add and element to dictionary 
+ex_1["Beer type"] = "Ale"
+print(ex_1)
+
+#We can also add a list to the dicionary
+ex_1["Wine type"] = ["White", "Red"]
+print(ex_1)
+
+#We can also add an element to the list 
+ex_1["Wine type"].append("Rose")
+print(ex_1)
+
+#If we try to check if the element is in the dictionary,
+# we can type 
+#ex_1("Cider")
+#but if it's not it will crash the program
+
+#so it's better to do it like this
+ex_1.get("Cider", "Sorry the element is not on the list")
+#now the program will return the value or the information that the 
+#element is not on the list
+
+#Now something really usefull
+#let's say we have variable called "Key"
+key = "Cofee"
+print(ex_1[key])
+#we will get the property
+#it's the same as
+print(ex_1["Cofee"])
+
+#toclear the dictonary ex_1.clear()
